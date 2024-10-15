379f2203e53c0345838a498cd1e3ad306cd9d64b
yuna <yuna744@gmail.com>
2024-10-16 06:13:01 +0900

    feat: 메세지 이름 안뜰때 username으로 대체

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,17 +1,18 @@
-import { Imessage } from '@/lib/stores/useMessagesStore';
 import Image from 'next/image';
 import MessageMenu from './MessageMenu';
 import { useAuthStore } from '@/lib/stores/useAuthStore';
+import { Imessage } from '@/lib/stores/useMessagesStore';
 
 export default function Message({ message }: { message: Imessage }) {
   const user = useAuthStore((state) => state.user);
+  const username = user?.user_metadata?.username;
 
   return (
     <div className='flex gap-2'>
       <div className='h-10 w-10 bg-green-500 rounded-full overflow-hidden'>
         <Image
           src={message.users?.avatar_url!}
-          alt={message.users?.display_name!}
+          alt={message.users?.display_name || username}
           width={40}
           height={40}
           className='rounded-full ring-2'
@@ -21,7 +22,7 @@ export default function Message({ message }: { message: Imessage }) {
       <div className='flex-1'>
         <div className='flex justify-between'>
           <div className='flex items-center gap-1'>
-            <h1 className='font-bold text-base'>{message.users?.display_name}</h1>
+            <h1 className='font-bold text-base'>{message.users?.display_name || username}</h1>
             <h1 className='text-sm text-gray-500'>{new Date(message.created_at).toDateString()}</h1>
             {message.is_edit && <h1 className='text-sm text-gray-400'>수정 완료</h1>}
           </div>
